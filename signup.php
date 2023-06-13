5f846815b45b83ff463a481090b9190fad91a90b
Wasif <wasifanton@gmail.com>
2023-06-13 16:50:04 +0300

    signup updates

--- signup.php
+++ signup.php
@@ -8,7 +8,6 @@ require_once './includes/header.php'
 <!-- Nav Bar -->
 <div class="container">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-
         <!-- Left side logo -->
         <div class="col-md-3 mb-2 mb-md-0">
             <a href="./index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
@@ -18,12 +17,11 @@ require_once './includes/header.php'
                 <span class="wwhite fs-4">Alcohol</span><span class="wweet fs-4">Archive</span>
             </a>
         </div>
-
         <!-- Middle -->
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
             <!-- Welcome -->
             <li class="nav-item">
-                <a class="wweet nav-link active" aria-current="page" href="#">Signup</a>
+                <a class="wweet nav-link active" aria-current="page" href="#"></a>
             </li>
         </ul>
         <!-- Login button -->
@@ -34,30 +32,56 @@ require_once './includes/header.php'
 </div>
 
 <!-- Signup Form -->
-<div class="container">
+<div class="container ">
     <div class="row justify-content-center">
         <div class="col-md-6">
             <div class="card">
-                <div class="card-header">
+                <div class="wweet card-header">
                     Signup
                 </div>
                 <div class="card-body">
                     <!-- Form -->
-                    <form>
+                    <form action="./includes/signup.inc.php" method="POST">
+                        <!-- Name -->
                         <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" placeholder="Enter your email">
+                            <label for="name" class="form-label"><strong>Name</strong></label>
+                            <input type="text" id="name" name="name" class="form-control" placeholder="Enter your name" required>
                         </div>
-
-                        <button type="submit" class="btn btn-primary">Login</button>
-                        <a type="button" href="./signup.php" class="btn ">Signup</a>
+                        <!-- Email -->
+                        <div class="mb-3">
+                            <label for="email" class="form-label"><strong>Email</strong></label>
+                            <input type="email" id="email" name="email" class="form-control" placeholder="Enter your email" required>
+                            <small id="idHelp" class="form-text text=muted"><strong>This will be your login</strong></small>
+                        </div>
+                        <!-- Phone -->
+                        <div class="col-mb-3">
+                            <label for="confirm_password" class="form-label"><strong>Phone Number</strong></label>
+                            <input type="phone" id="phone" name="phone" class="form-control" placeholder="Enter your phone number">
+                        </div>
+                        <br>
+                        <!-- Password -->
+                        <div class="mb-3">
+                            <label for="password" class="form-label"><strong>Password</strong></label>
+                            <input type="password" id="password" name="password" autocomplete="new-password" class=" form-control" placeholder="Enter your password" required>
+                            <span id="passwordHelpInline" class="form-text">
+                                Must not contain spaces, emoji.
+                        </div>
+                        <!-- Confirm Password -->
+                        <div class="col-mb-3">
+                            <label for="confirm_password" class="form-label"><strong>Confirm Password</strong></label>
+                            <input type="password" id="confirm_password" name="confirm_password" autocomplete="new-password" class=" form-control" placeholder="Confirm your password">
+                        </div>
+                        <br>
+                        <button type="submit" class="btn btn-primary">Create your account</button>
+                        <p class="text-gray-soft small mb-2"><strong>Already have an account? </strong><a href="./login.php">Login</a></p>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
-    <!-- Footer -->
-    <?php
-    require_once './include/footer.php'
-    ?>
+<!-- Footer -->
+<?php
+require_once './include/footer.php'
+?>
