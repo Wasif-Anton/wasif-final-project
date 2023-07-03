f95cfda38ca9c3aec7bba0ecad34876fe20c70ba
wasif <wasifanton@gmail.com>
2023-07-03 13:31:04 +0300

    Nothing

--- login.php
+++ login.php
@@ -67,11 +67,3 @@ require_once './includes/header.php'
     require_once './includes/footer.php'
     ?>
 
-    <!-- Log in with Google -->
-    <!-- 
-    // Set the client ID and client secret
-    $client->setClientId('785062031643-27are9o7k0i9mtooqct3at7f2kc8emk8.apps.googleusercontent.com');
-    $client->setClientSecret('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');
-    // Set the redirect URI
-    $client->setRedirectUri('http://localhost/wasif-final-project/login.php');
-    -->
